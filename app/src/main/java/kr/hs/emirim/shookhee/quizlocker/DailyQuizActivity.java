b75e336bae3491cfab6235b7c6e44b49e0d3352d
Minji Jeong <s2018s38@e-mirim.hs.kr>
2019-10-16 21:28:17 +0900

    backup2

---
+++ app/src/main/java/kr/hs/emirim/shookhee/quizlocker/DailyQuizActivity.java
@@ -0,0 +1,14 @@
+package kr.hs.emirim.shookhee.quizlocker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DailyQuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily_quiz);
+    }
+}
