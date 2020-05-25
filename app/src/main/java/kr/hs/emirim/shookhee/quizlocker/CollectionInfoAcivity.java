87f1cb4bed1b997adbaf4788e81babb550b1ade2
Sookhee <s2018s38@e-mirim.hs.kr>
2020-05-26 01:21:31 +0900

    adapter intent putExtra 데이터 표시

--- app/src/main/java/kr/hs/emirim/shookhee/quizlocker/CollectionInfoAcivity.java
+++ app/src/main/java/kr/hs/emirim/shookhee/quizlocker/CollectionInfoAcivity.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class CollectionInfoAcivity extends AppCompatActivity {
-
     String name;
     String info;
     int img;
@@ -17,12 +20,21 @@ public class CollectionInfoAcivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_collection_info_acivity);
 
+        ImageView ivCarrotImage = (ImageView)findViewById(R.id.carrotImageView);
+        Button btnCarrotName = (Button)findViewById(R.id.carrotNameButton);
+        TextView tvCarrotInfo = (TextView)findViewById(R.id.carrotInfoTextView);
+
         Intent intent = getIntent();
         name = intent.getExtras().getString("name");
         info = intent.getExtras().getString("info");
         img = intent.getExtras().getInt("img");
 
-        Toast.makeText(getApplicationContext(), name + " " + info + " " + img, Toast.LENGTH_SHORT).show();
+        ivCarrotImage.setBackgroundResource(img);
+        btnCarrotName.setText(name);
+        tvCarrotInfo.setText(info);
+    }
 
+    public void onBackClick(View view){
+        super.onBackPressed();
     }
 }
