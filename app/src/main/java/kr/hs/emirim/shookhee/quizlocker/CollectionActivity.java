3cd72bc9343be20809d3c6f9b2f17e99503832d2
jiwonan <s2018s31@e-mirim.hs.kr>
2020-06-08 22:50:41 +0900

    도감 정보 화면 버튼 연결

--- app/src/main/java/kr/hs/emirim/shookhee/quizlocker/CollectionActivity.java
+++ app/src/main/java/kr/hs/emirim/shookhee/quizlocker/CollectionActivity.java
@@ -2,7 +2,11 @@ package kr.hs.emirim.shookhee.quizlocker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
 
 public class CollectionActivity extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class CollectionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_collection);
+
+    }
+
+    public void onCollectionInfoBtn(View v) {
+        Intent intent = new Intent(getApplicationContext(), CollectionInfoAcivity.class);
+        startActivity(intent);
     }
 }
